05a30150ec64acb3f5550ef5a1daef794113c62c
ramennn <ramendrakahu@gmail.com>
2016-09-28 20:58:11 +0530

    Update TestBankAccount.java

--- jenkins-master/src/test/java/ca/uwo/csd/TestBankAccount.java
+++ jenkins-master/src/test/java/ca/uwo/csd/TestBankAccount.java
@@ -12,4 +12,11 @@ public class TestBankAccount {
     double amount = account.debit(5);
     Assert.assertEquals(5.0,amount);
   }
+  
+   @Test
+  public void testDebitWithInSufficientFunds(){
+    BankAccount account = new BankAccount(11);
+    double amount = account.debit(5);
+    Assert.assertEquals(10.0,amount);
+  }
 }
